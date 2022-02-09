bfdf049efa4e4d83d2fca862ca482f7c0f447de8
Oussama_BENLAKHDAR <oussamaoussamaben@gmail.com>
2022-02-09 23:10:41 +0100

    Modifiez le programme principal

--- src/MainApp.java
+++ src/MainApp.java
@@ -4,7 +4,8 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		InterfaceDBConnexion conn = DBConnection.getInstance();
+		
 
 		EtudiantService serv=new EtudiantService();
 		try {
